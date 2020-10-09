eedf146e9e8e536a62dd284d4114757bc833cf5b
rakesh <kallurirakesh15@gmail.com>
2020-10-09 14:07:56 -0400

    added semantic styling and implemented lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,18 @@ import { ElementsModule } from './elements/elements.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ElementsModule,
     CollectionsModule,
     ViewsModule,
     ModsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

73cd4b163860228df901e771f298958c919f3246
rakesh <kallurirakesh15@gmail.com>
2020-10-09 10:10:16 -0400

    added modules and basic routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { ViewsModule } from './views/views.module';
+import { ModsModule } from './mods/mods.module';
+import { CollectionsModule } from './collections/collections.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ElementsModule } from './elements/elements.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
+    ViewsModule,
+    ModsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
-
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    CollectionsRoutingModule
-  ]
+  declarations: [CollectionsHomeComponent],
+  imports: [CommonModule, CollectionsRoutingModule],
 })
-export class CollectionsModule { }
+export class CollectionsModule {}

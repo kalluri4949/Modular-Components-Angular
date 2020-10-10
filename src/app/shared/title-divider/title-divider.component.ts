9591fd05400abe39b13171fbd75a90af45c4f6cc
rakesh <kallurirakesh15@gmail.com>
2020-10-10 17:39:02 -0400

    added reusable widgets using ng-content

---
+++ src/app/shared/title-divider/title-divider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-divider',
+  templateUrl: './title-divider.component.html',
+  styleUrls: ['./title-divider.component.css']
+})
+export class TitleDividerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

a5de0ed36e97624eb66dd8fa29e2031aa95b44ed
rakesh <kallurirakesh15@gmail.com>
2020-10-12 09:02:00 -0400

    Reusable Table component on collections modue

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
-
+data = [
+  {
+    name: 'rakesh',
+    age: 27,
+    job: 'Ui Developer'
+  },
+  {
+    name: 'veena',
+    age: 27,
+    job: 'Front End Developer'
+  },
+  {
+    name: 'venky',
+    age: 28,
+    job: 'React Developer'
+  },
+];
+headers = [
+  {
+    key: 'name',
+    label: 'Name'
+  },
+  {
+    key: 'age',
+    label: 'Age'
+  },
+  {
+    key: 'job',
+    label: 'Job'
+  },
+];
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/collections/table/table.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+// tslint:disable-next-line:no-input-rename
+@Input('class')classNames = '';
+@Input()data = [];
+@Input()headers = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

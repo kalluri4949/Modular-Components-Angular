50930dda269a1f13cf5268f6603b0a570b65f0a0
rakesh <kallurirakesh15@gmail.com>
2020-10-13 13:53:04 -0400

    Added Accordian component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
 openModal = false;
+items = [
+  {
+    title: 'What is a dog?',
+    content: 'A dog is a type of domesticated animal'
+  },
+  {
+    title: 'What kinds of dogs are there?',
+    content: 'There are many breeds of dogs. Each breed varies in size and temperament.'
+  },
+  {
+    title: 'How do you acquire a dog?',
+    content: 'Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.'
+  }
+];
   constructor() { }
 
   ngOnInit(): void {
